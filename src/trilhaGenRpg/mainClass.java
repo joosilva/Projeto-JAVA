89b50c30d27c4c6974b54931ad785dffc78a47fb
joosilva <joaovictormendessilva1@gmail.com>
2023-02-24 22:41:08 -0300

    Testando métodos.

--- src/trilhaGenRpg/mainClass.java
+++ src/trilhaGenRpg/mainClass.java
@@ -3,8 +3,30 @@ package trilhaGenRpg;
 public class mainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Inventario inventario = new Inventario ();
+		
+        inventario.criarPontos();
+        inventario.ganharPontos("Comunicação", 2);
+        inventario.ganharPontos("Empatia", 3);
+        inventario.ganharPontos("Comunicação", 6);
+        inventario.ganharPontos("Trabalho em Equipe", 3);
+        inventario.ganharPontos("Comunicação", -1);
+        inventario.ganharPontos("Proatividade", 2);
+        inventario.imprimirPontos();
+        
+        inventario.criarItens();
+        inventario.ganharMoedas(10);
+        inventario.comprarItem(2);
+        //inventario.adicionarChave();
+        
+        if (inventario.verificarMochila("Chave")) {
+        	System.out.println("\nVocê tem chave.");
+        } else {
+        	System.out.println("\nVocê não tem chave.");
+        }
+        
+		
 	}
 
 }
