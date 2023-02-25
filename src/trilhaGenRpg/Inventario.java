801fdb8b67a2e73bb9b9b1c267bc68f7e1248b22
joosilva <joaovictormendessilva1@gmail.com>
2023-02-24 22:21:46 -0300

    Métodos de funcionalidade de jovo (v2).

--- src/trilhaGenRpg/Inventario.java
+++ src/trilhaGenRpg/Inventario.java
@@ -7,7 +7,7 @@ public class Inventario {
 
 	ArrayList<String> mochila = new ArrayList<String>();
 	List<Item> itens = new ArrayList<Item>();
-	List<Pontos> pontuacao;
+	List<Pontos> pontuacao = new ArrayList<Pontos>();
 	private int moedas = 2;
 	private String florDaAmizade;
 	private String florDaPaz;
@@ -86,7 +86,7 @@ public class Inventario {
 		this.chave = chave;
 	}
 
-	public void ganharPontos(String tipoPonto, int quantidadePonto) {
+	public void criarPontos() {
 		Pontos comunicacao = new Pontos("Comunicação", 0);
 		Pontos proatividade = new Pontos("Proatividade", 0);
 		Pontos orientacaoAoDetalhe = new Pontos("Orientação ao Detalhe", 0);
@@ -94,28 +94,6 @@ public class Inventario {
 		Pontos empatia = new Pontos("Empatia", 0);
 		Pontos trabalhoEmEquipe = new Pontos("Trabalho em Equipe", 0);
 
-		switch (tipoPonto) {
-		case "Comunicação":
-			comunicacao.setValor(comunicacao.getValor() + quantidadePonto);
-			break;
-		case "Proatividade":
-			proatividade.setValor(proatividade.getValor() + quantidadePonto);
-			break;
-		case "Orientação ao Detalhe":
-			comunicacao.setValor(orientacaoAoDetalhe.getValor() + quantidadePonto);
-			break;
-		case "Responsabilidade Pessoal":
-			responsabilidadePessoal.setValor(responsabilidadePessoal.getValor() + quantidadePonto);
-			break;
-		case "Empatia":
-			empatia.setValor(empatia.getValor() + quantidadePonto);
-			break;
-		case "Trabalho em Equipe":
-			trabalhoEmEquipe.setValor(trabalhoEmEquipe.getValor() + quantidadePonto);
-			break;
-			
-		}
-		
 		pontuacao.add(comunicacao);
 		pontuacao.add(proatividade);
 		pontuacao.add(orientacaoAoDetalhe);
@@ -125,7 +103,33 @@ public class Inventario {
 
 	}
 	
+	public List<Pontos> ganharPontos(String tipoPonto, int quantidadePontos) {
+		
+		switch (tipoPonto) {
+		case "Comunicação":
+			pontuacao.get(0).setValor(pontuacao.get(0).getValor() + quantidadePontos);
+			break;
+		case "Proatividade":
+			pontuacao.get(1).setValor(pontuacao.get(1).getValor() + quantidadePontos);
+			break;
+		case "Orientação ao Detalhe":
+			pontuacao.get(2).setValor(pontuacao.get(2).getValor() + quantidadePontos);
+			break;
+		case "Responsabilidade Pessoal":
+			pontuacao.get(3).setValor(pontuacao.get(3).getValor() + quantidadePontos);
+			break;
+		case "Empatia":
+			pontuacao.get(4).setValor(pontuacao.get(4).getValor() + quantidadePontos);
+			break;
+		case "Trabalho em Equipe":
+			pontuacao.get(5).setValor(pontuacao.get(5).getValor() + quantidadePontos);
+			break;
+		
+		}
+		
+		return pontuacao;
 
+	}
 
 	public void imprimirPontos() {
 		System.out.println("\nVocê finalizou com um total de:");
