11ccc0a84fc63feed2b31ba4bec613f318625b17
joosilva <joaovictormendessilva1@gmail.com>
2023-02-24 23:28:28 -0300

    Arte em ASCII do menu.

---
+++ src/trilhaGenRpg/menu.java
@@ -0,0 +1,17 @@
+package trilhaGenRpg;
+
+public class menu {
+
+	public static void main(String[] args) {
+		
+		System.out.println("( )_      _(_ )( )");
+		System.out.println("|  _)_ __(_)| || |__    _ _      __    __   ___");
+		System.out.println("| | (  __) || ||  _  \\/ _  )   / _  \\/ __ \\  _  \\");
+		System.out.println("| |_| |  | || || | | | (_| |  ( (_) |  ___/ ( ) |");
+		System.out.println(" \\__)_)  (_)___)_) (_)\\__ _)   \\__  |\\____)_) (_)");
+		System.out.println("                              ( )_) |");
+		System.out.println("                               \\___/");
+			
+	}
+
+}
