a60656265543c7bfe405d71da435cd347f67ed58
joosilva <joaovictormendessilva1@gmail.com>
2023-02-25 15:56:25 -0300

    Novos testes de métodos.

--- src/trilhaGenRpg/mainClass.java
+++ src/trilhaGenRpg/mainClass.java
@@ -1,10 +1,22 @@
 package trilhaGenRpg;
 
+import java.util.Scanner;
+
 public class mainClass {
 
 	public static void main(String[] args) {
 		
-		Inventario inventario = new Inventario ();
+		Scanner leia = new Scanner(System.in);
+				
+		Inventario inventario = new Inventario ();	
+		
+		inventario.ganharMoedas(10);
+		
+		System.out.println(inventario.criarItens().get(0).getNome() + inventario.criarItens().get(0).getValor());
+		System.out.println("Item 1 - poção\nItem 2 - pingente");
+		int opcao = leia.nextInt();
+		
+		inventario.comprarItem(opcao);
 		
         inventario.criarPontos();
         inventario.ganharPontos("Comunicação", 2);
@@ -18,15 +30,14 @@ public class mainClass {
         inventario.criarItens();
         inventario.ganharMoedas(10);
         inventario.comprarItem(2);
-        //inventario.adicionarChave();
+        //inventario.adicionarObjeto("Chave");
         
         if (inventario.verificarMochila("Chave")) {
         	System.out.println("\nVocê tem chave.");
         } else {
         	System.out.println("\nVocê não tem chave.");
         }
-        
-		
+      
 	}
 
 }
